36384e74b4911013d28ef56defa5d8a6d6c0a813
Santiago <grecodominguezsantiago@gmail.com>
2026-05-22 20:13:13 +0200

    Header multi-idioma

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import LanguageSelector from "@/components/i18n/languale-selector";
 import ThemeSelector from "@/components/i18n/theme-selector";
+import { getMessages, getTranslations } from "next-intl/server";
 
-export default function Header() {
+export default async function Header() {
+  const t = await getTranslations("Header");
+
   return (
     <header className="flex items-center justify-between max-w-260 w-full mt-16 mb-4 px-4">
       <nav className="flex gap-4 text-lg">
-        <Link href="/projects">Inicio</Link>
-        <Link href="/projects">Proyectos</Link>
-        <Link href="/tecnologies">Tecnologías</Link>
-        <Link href="/profile">Profile</Link>
+        <Link href="/projects">{t("home")}</Link>
+        <Link href="/projects">{t("projects")}</Link>
+        <Link href="/tecnologies">{t("technologies")}</Link>
+        <Link href="/profile">{t("profile")}</Link>
       </nav>
 
       <section className="flex gap-4 text-lg">
