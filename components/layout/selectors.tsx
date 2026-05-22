0288c01e1aa534f3993443b6349c68ddb56972e0
Santiago <grecodominguezsantiago@gmail.com>
2026-05-22 19:46:56 +0200

    Selector de tono funcional

--- components/layout/selectors.tsx
+++ components/layout/selectors.tsx
@@ -1,9 +1,11 @@
 import LanguageSelector from "@/components/i18n/languale-selector";
+import ThemeSelector from "../i18n/theme-selector";
 
 export default function Selectors() {
   return (
-    <section className="absolute top-4 right-4">
+    <section className="flex gap-4 absolute top-4 right-4 text-lg">
       <LanguageSelector />
+      <ThemeSelector />
     </section>
   );
 }
